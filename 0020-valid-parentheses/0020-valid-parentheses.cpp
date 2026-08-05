433b10c99e54b8fe69109f2af31728a95352ad0b
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-08-05 17:07:07 +0530

    Time: 0 ms (100%), Space: 8.8 MB (68.3%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -22,3 +22,30 @@ public:
         return st.empty();
     }
 };
+/* 
+optimal-->
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+
+            if (c == '(')
+                st.push(')');
+            else if (c == '[')
+                st.push(']');
+            else if (c == '{')
+                st.push('}');
+            else {
+                if (st.empty() || st.top() != c)
+                    return false;
+
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
+*/
