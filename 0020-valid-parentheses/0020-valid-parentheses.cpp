edbd836582ac706b97c03e7778668b9d4ff0c248
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-08-05 16:58:22 +0530

    Time: 2 ms (14.83%), Space: 10 MB (8.86%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> a;
+        for(auto x:s){
+            if(!a.empty()){
+                if(a.top()=='(' && x==')'|| a.top()=='[' && x==']' || a.top()=='{' && x=='}'){
+                    a.pop();
+                }
+                else
+                    a.push(x); 
+            }
+            else    
+                a.push(x);
+        }
+        if(a.empty())   
+            return true;
+        return false;
+    }
+};
