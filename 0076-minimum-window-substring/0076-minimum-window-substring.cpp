d16e1679d49f3b0bfeb587f6be4a97b173da524a
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-08-16 20:20:48 +0530

    Time: 3 ms (78.09%), Space: 11.6 MB (43.51%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        vector<int>thash(128,0);
-        vector<int>wind(128,0);
-        int left=0,right=0,req=0,form=0;
-        int minl = INT_MAX,start=0;
         int m=s.size();
         int n=t.size();
+        int left=0,right=0,req=0,form=0;
+        int minl = INT_MAX,start=0;
+        vector<int>thash(128,0);
+        vector<int>wind(128,0);
         if(n>m)
             return "";
-        for(char c : t)
-            thash[c]++; 
-
-        for (int i = 0; i < 128; i++)
-        {
-            if (thash[i] > 0)
-                req++;
-        }
-        
-        while(right < s.size()){
+        for(char c:t)
+            thash[c]++;
+        for (int i = 0; i < 128; i++){ 
+            if (thash[i] > 0) 
+                req++; 
+        }    
+        while(right<s.size()){
             char c = s[right];
             wind[c]++;
-            if (wind[c] == thash[c])
+            if(wind[c]==thash[c])
                 form++;
-            while(form == req)
-            {
+            while(form==req){
                 if(right-left+1 < minl){
                     minl=right-left+1;
                     start=left;
                 }
                 wind[s[left]]--;
+                //checking decrese in hash value
                 if(wind[s[left]] < thash[s[left]])
                     form--;
                 left++;
             }
             right++;
         }
-        
         if (minl == INT_MAX)
             return "";
         return s.substr(start,minl);
     }
+    
 };
