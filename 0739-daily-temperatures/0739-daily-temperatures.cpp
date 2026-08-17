af54a93c06826ab69df1130e3277f1348ad56e41
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-08-17 17:34:09 +0530

    Time: 32 ms (16.85%), Space: 111.9 MB (6.16%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temp) {
-        stack<pair<int, int>> st;
+        stack<pair<int,int>>st;
         int n=temp.size();
         vector<int>arr(n,0);
         for(int i=0;i<n;i++){
-                while (!st.empty() && st.top().second < temp[i]){
-                    auto a=st.top();
-                    st.pop();
-                    arr[a.first] = i - a.first;
-                }
-                st.push({i,temp[i]});
+            while(!st.empty() && st.top().second < temp[i]){
+                auto a=st.top();
+                st.pop();
+                //for storing in that exact index
+                arr[a.first] = i - a.first;
             }
+            st.push({i,temp[i]});
+        }
         return arr;
     }
 };
