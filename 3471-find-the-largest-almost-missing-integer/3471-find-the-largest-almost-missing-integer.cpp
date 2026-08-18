c59da868b00aac54b6b8f54aad42022184c01155
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-08-19 02:40:48 +0530

    Time: 3 ms (54.18%), Space: 29.1 MB (80.36%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -7,6 +7,12 @@ public:
         for (int i : nums)
             ++freq[i];
         int ans = -1;
+        /*if freq=1
+          case 1: first element
+          case 2: last element
+          case 3: window size 1
+          case 4: a case where window size is equal to size of array
+        */
         for (int i = 0; i < n; i++) {
             if (k == n || (freq[nums[i]] == 1 && (k == 1 || i == 0 || i == n - 1)))
                 ans = max(ans, nums[i]);
