db5f6a15d4692a6eff6643db5fd367da1f060b5a
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-08-20 00:56:03 +0530

    Time: 0 ms (100%), Space: 7.8 MB (82.68%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,5 +1,29 @@
 class Solution {
 public:
+    //optimal
+     int nextNumber(int n) {
+        int sum = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+
+        return sum;
+    }
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = nextNumber(n);
+
+        while (fast != 1 && slow != fast) {
+            slow = nextNumber(slow);
+            fast = nextNumber(nextNumber(fast));
+        }
+        return fast == 1;
+    }
+
+/*
     bool isHappy(int n) {
        unordered_set<int>seen;
        while(n!=1){
@@ -17,4 +41,5 @@ public:
        }
        return true;
     }
+*/
 };
