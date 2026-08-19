9cb514543b0c3270be77196d5d3aeeb120055f08
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-08-19 22:01:21 +0530

    Time: 0 ms (100%), Space: 22.8 MB (78.13%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -1,51 +1,44 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        ListNode* slow=head;
-        ListNode* fast=head;
-        //finding mid point
-        while(fast->next!=nullptr && fast->next->next!= nullptr){
-            slow=slow->next;
-            fast=fast->next->next;
+        if (!head || !head->next) return;
+
+        // 1. Find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        //dividing into halves
-        ListNode* second = slow->next;//SECOND HALF VALUE
-        slow->next = nullptr; // CUTS THE LIST INTO TWO HALVES
+
+        // 2. Split into two halves
+        ListNode* second = slow->next;
+        slow->next = nullptr;
+
+        // 3. Reverse second half
         ListNode* prev = nullptr;
-        ListNode* curr = second;
-        //reversing second half
-        while(curr!=nullptr){
-            ListNode* Next = curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=Next;
+
+        while (second) {
+            ListNode* next = second->next;
+            second->next = prev;
+            prev = second;
+            second = next;
         }
+
+        // 4. Merge the two halves
         ListNode* first = head;
         second = prev;
-        //merging halves
-        while (second != nullptr) {
-            // 1. Save
-            ListNode* temp1 = first->next;
-            ListNode* temp2 = second->next;
 
-            // 2. Connect
+        while (second) {
+            ListNode* firstNext = first->next;
+            ListNode* secondNext = second->next;
+
             first->next = second;
-            second->next = temp1;
+            second->next = firstNext;
 
-            // 3. Move
-            first = temp1;
-            second = temp2;
-        }    
-  
+            first = firstNext;
+            second = secondNext;
+        }
     }
 };
