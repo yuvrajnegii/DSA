d80d66803c7cd13fb736ad1992593eff5dd41960
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-08-27 01:27:51 +0530

    Time: 0 ms (100%), Space: 23.9 MB (55.98%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -12,3 +12,27 @@ public:
         }
     }
 };
+/*
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int first = 0, second = 1;
+
+        while (second < n) {
+            if (!nums[first] && nums[second]) {
+                swap(nums[first], nums[second]);
+                first++;
+                second++;
+            }
+            else if (!nums[first] && !nums[second]) {
+                second++;
+            }
+            else {
+                first = second;
+                second++;
+            }
+        }
+    }
+};
+*/
