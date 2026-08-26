f76631df88b8e737cfa93abac278036b541026ef
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-08-27 01:10:18 +0530

    Time: 131 ms (14.37%), Space: 142.2 MB (8.29%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int>dups;
+        for(int i=0;i<nums.size();i++){
+            if(dups.count(nums[i])){
+                int check=abs(dups.at(nums[i])-i);
+                if(check <= k)
+                    return true;
+            }
+            dups[nums[i]]=i;
+        }
+        return false;
+    }
+};
