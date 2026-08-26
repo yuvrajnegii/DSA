5f82ad69648f56c7d1a332ea3a46780496f2bc0f
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-08-27 00:03:14 +0530

    Time: 4 ms (30.46%), Space: 8.5 MB (64.84%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-
-        // Negative numbers are not palindromes
-        // Numbers ending in 0 are not palindromes (except 0 itself)
-        if (x < 0 || (x % 10 == 0 && x != 0))
+        if (x < 0)
             return false;
-
-        int reversedHalf = 0;
-
-        while (x > reversedHalf) {
-            reversedHalf = reversedHalf * 10 + x % 10;
-            x /= 10;
+        long rev=0,temp=x;
+        while(temp){
+            long num=temp%10;
+            rev=rev*10+num;
+            temp/=10;
         }
-
-        // Even number of digits: x == reversedHalf
-        // Odd number of digits: ignore middle digit
-        return x == reversedHalf || x == reversedHalf / 10;
+        return (rev==long(x));
     }
 };
