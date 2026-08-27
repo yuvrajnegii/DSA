be6260ddda5fc3682a72f8835c9689f0142c6fca
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-08-27 22:30:03 +0530

    Time: 0 ms (100%), Space: 13.9 MB (49%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        string s;
+        if(chars.size()==1)
+            return 1;
+            int read=0,count=1;
+            s+=chars[read];
+        for(int write=1;write<chars.size();write++)
+        {
+            if(chars[read]==chars[write] )
+                count++;
+            else{
+                if(count>1)
+                    s+=to_string(count);
+                s+=chars[write];
+                read=write;
+                count=1;
+            }
+        }
+        if(count>1)
+            s+=to_string(count);
+        for(int i=0;i<s.size();i++){
+            chars[i]=s[i];
+        }
+        return s.size();    
+    }
+};
