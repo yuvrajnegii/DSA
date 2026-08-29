472b9c8fecb3aba5013a5752acb252feac5299d2
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-08-30 02:42:18 +0530

    Time: 0 ms (100%), Space: 9.8 MB (29.19%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -4,6 +4,7 @@ public:
         vector<vector<int>>result(numRows);
         for(int i=0;i<numRows;i++){
             result[i]=vector<int>(i+1,1);//i+1 size
+            
             for(int j=1;j<i;j++){
                 result[i][j]=result[i-1][j]+result[i-1][j-1];
             }
