3428cb9f53fb751c0236686cab727f6ccc15c85a
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-08-30 02:15:35 +0530

    Time: 0 ms (100%), Space: 10.1 MB (93.49%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -15,7 +15,6 @@ public:
         for(int i=0;i<n/2;i++){
             for(int j=0;j<n;j++){
                 swap(matrix[j][i],matrix[j][n-1-i]);
-                //or reverse(m[i].begin(),m[i].end());
             }
         }
         */
