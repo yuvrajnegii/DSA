c27d0c0f7cd09309aa29c9b961ca20b50abb9ada
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-08-30 02:06:28 +0530

    Time: 20 ms (80.11%), Space: 88.4 MB (42.93%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -6,18 +6,18 @@ public:
         //map structure: {prefix_sum,index}
         mp[0]=-1;
         //0 sum and with index at -1 (not yet started)
-        int prefix=0,count=0;
+        int prefix=0,maxl=0;
         for(int i=0;i<n;i++){
             //if not 1 add -1
             prefix+=(nums[i]==1) ? 1 : -1;
             if(mp.find(prefix)!=mp.end()){
                 int size=i-mp[prefix];
-                count=max(count,size);
+                maxl=max(maxl,size);
             }
             else{
                 mp[prefix]=i;
             }
         }
-        return count;
+        return maxl;
     }
 };
