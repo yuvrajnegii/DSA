07eb2b300b105db4b5549838d9e23c891d490616
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-09-01 16:00:01 +0530

    Time: 0 ms (100%), Space: 11.7 MB (48.86%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(auto x:nums)
-            mp[x]++;
-        int i=0;
-        for(int color=0 ; color <= 2 ; color++){
-            for(int j=0; j < mp[color] ; j++){
-            nums[i] = color;
-            i++;
+        int count[3] = {0};
+
+        // Count 0s, 1s, and 2s
+        for (int num : nums) {
+            count[num]++;
+        }
+        // Overwrite nums
+        int i = 0;
+
+        for (int color = 0; color < 3; color++) {
+            for (int j = 0; j < count[color]; j++) {
+                nums[i] = color;
+                i++;
             }
         }
     }
