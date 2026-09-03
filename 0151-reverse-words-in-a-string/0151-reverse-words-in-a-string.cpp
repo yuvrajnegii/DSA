5b743553371d61189387b63a4a32529c0f0a32c3
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-09-04 03:26:31 +0530

    Time: 0 ms (100%), Space: 10.6 MB (44.69%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -3,24 +3,20 @@ public:
     string reverseWords(string s) {
         reverse(s.begin(), s.end());
         s += ' ';
-
         string sen = "";
         string rep = "";
-
         for (auto x : s) {
             if (x == ' ') {
-                if (!rep.empty()) {              // ADD THIS
+                if (!rep.empty()) {           
                     reverse(rep.begin(), rep.end());
                     sen += rep;
                     sen += ' ';
                     rep = "";
                 }
             }
-            else {
-                rep += x;
-            }
+            else 
+                rep += x;  
         }
-
         sen.pop_back();
         return sen;
     }
