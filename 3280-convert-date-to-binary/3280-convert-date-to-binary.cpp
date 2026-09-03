1dfc9deea0cdb672b0f46b4e2208383760f0e256
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-09-03 17:28:23 +0530

    Time: 0 ms (100%), Space: 8.3 MB (85.9%) - LeetHub

--- 3280-convert-date-to-binary/3280-convert-date-to-binary.cpp
+++ 3280-convert-date-to-binary/3280-convert-date-to-binary.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
-
-    string binary(int n) {
-        if(n == 0)
-            return "0";
-
-        string s = "";
-
-        while(n > 0) {
-            s += (n % 2) + '0';
-            n /= 2;
-        }
-
-        reverse(s.begin(), s.end());
-
-        return s;
-    }
-
+    
     string convertDateToBinary(string date) {
-        string ans = "";
-        string num = "";
-
-        for(char c : date) {
-            if(c == '-') {
-                ans += binary(stoi(num));
-                ans += '-';
-                num = "";
+        string s="";
+        string b="";
+        date+='-';
+        for(auto x:date){
+            if(x == '-'){
+                int a=stoi(b);
+                b=bitset<32>(a).to_string();
+                s+=b.substr(b.find('1'));
+                b="";
+                s+=x;
             }
-            else {
-                num += c;
+            else{
+                b+=x;
             }
         }
-
-        ans += binary(stoi(num));
-
-        return ans;
+        s.pop_back();
+        return s;
     }
 };
