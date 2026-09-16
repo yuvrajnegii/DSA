3bab824fabf407a9a1f19cc1009986c25228b21a
Yuvraj Negi <yuvnegiraj2101@gmail.com>
2026-09-16 21:38:48 +0530

    Time: 17 ms (16.82%), Space: 46.6 MB (24.9%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer>mp= new HashMap<>();
+        if (s.length() != t.length()) {
+            return false;
+        }
+        for(char c:s.toCharArray()){
+            if(mp.containsKey(c))
+                mp.put(c,mp.get(c)+1);
+            else
+                mp.put(c,1);
+        }
+        for(char c:t.toCharArray()){
+            if(!mp.containsKey(c)){
+                return false;
+            }
+            mp.put(c,mp.get(c)-1);
+            if(mp.get(c) == 0){
+                mp.remove(c);
+            }
+        }
+        return mp.isEmpty();
+    }
+}
